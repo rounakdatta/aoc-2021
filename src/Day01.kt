90d9efc9d0ca35050acb15ea46d7cab5418afb7c
Rounak Datta <rounakdatta12@gmail.com>
2022-09-25 19:22:46 +0530

    It's always day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.zipWithNext { firstElement, secondElement ->
+            secondElement.toInt() > firstElement.toInt()
+        }
+                .filter { it }
+                .size
+
+        // note that we could have used .windowed as well
+        // watch me make use of .windowed in the next part
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        return input.windowed(3).map { it ->
+            it.map { _it ->
+                _it.toInt()
+            }
+                    .sum()
+        }
+                .zipWithNext { first, second ->
+                    second > first
+                }
+                .filter { it }
+                .size
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    }
 
     val input = readInput("Day01")
     println(part1(input))
